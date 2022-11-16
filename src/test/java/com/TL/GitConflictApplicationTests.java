7c1a17a4c512947b23d432785ac0668f0f086901
rushibagul0909 <rushibagul0909@gmail.com>
2022-11-16 11:54:57 +0530

    TL Added

---
+++ src/test/java/com/TL/GitConflictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitConflictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
